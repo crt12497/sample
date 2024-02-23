eff00ed2bc46fa978a650fd1d497b3464250fe9a
RaviTeja <104969264+crt12497@users.noreply.github.com>
2024-02-23 13:17:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const openCameraButton = document.getElementById('openCamera');
+const videoElement = document.getElementById('preview');
+
+let scanner = new Instascan.Scanner({ video: videoElement });
+
+openCameraButton.addEventListener('click', function() {
+  Instascan.Camera.getCameras().then(function(cameras) {
+    if (cameras.length > 0) {
+      scanner.start(cameras[0]);
+      videoElement.style.display = 'block'; // Show the video element
+    } else {
+      console.error('No cameras found.');
+    }
+  }).catch(function(e) {
+    console.error(e);
+  });
+});
+
+scanner.addListener('scan', function(content) {
+  alert('Scanned: ' + content);
+});
